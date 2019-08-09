7a1796e0e88514927173f170f044ae0f4d1dd01a
abrahan56 <abrahanencarnacion02@gmail.com>
2019-08-09 17:10:58 -0400

    Create addinginputstotables.sql

---
+++ addinginputstotables.sql
@@ -0,0 +1,94 @@
+-- Begin Inserting Data
+
+-- --------------------------------------------------
+-- Insert into abrahan.departments table
+-- --------------------------------------------------
+insert into abrahan.departments values ('Clothing','Home');
+insert into abrahan.departments values ('Grocery','Home');
+insert into abrahan.departments values ('Decor','Home');
+insert into abrahan.departments values ('Furniture','Home');
+insert into abrahan.departments values ('Computers','Electronics');
+insert into abrahan.departments values ('Device Repair','Electronics');
+insert into abrahan.departments values ('Phones & Tablets','Electronics');
+insert into abrahan.departments values ('Garden','Outdoors');
+insert into abrahan.departments values ('Camping & Fishing','Outdoors');
+insert into abrahan.departments values ('Sports','Outdoors');
+insert into abrahan.departments values ('Children Clothing','Kids');
+insert into abrahan.departments values ('Toys','Kids');
+insert into abrahan.departments values ('Vitamins','Health');
+insert into abrahan.departments values ('Pharmacy','Health');
+insert into abrahan.departments values ('First Aid','Health');
+insert into abrahan.departments values ('Automotive','Hardware');
+insert into abrahan.departments values ('Tools','Hardware');
+insert into abrahan.departments values ('Jewelry','Fashion');
+insert into abrahan.departments values ('Beauty','Fashion');
+insert into abrahan.departments values ('Cosmetics','Fashion');
+insert into abrahan.departments values ('Books','Entertainment');
+insert into abrahan.departments values ('Games','Entertainment');
+insert into abrahan.departments values ('Music','Entertainment');
+insert into abrahan.departments values ('Movies','Entertainment');
+
+-- --------------------------------------------------
+-- Insert into abrahan.regions table
+-- --------------------------------------------------
+insert into abrahan.regions values (1, 'Southwest', 'United States');
+insert into abrahan.regions values (2, 'Northeast', 'United States');
+insert into abrahan.regions values (3, 'Northwest', 'United States');
+insert into abrahan.regions values (4, 'Central', 'Asia');
+insert into abrahan.regions values (5, 'East Asia', 'Asia');
+insert into abrahan.regions values (6, 'Quebec', 'Canada');
+insert into abrahan.regions values (7, 'Nova Scotia', 'Canada');
+
+-- --------------------------------------------------
+-- Insert into abrahan.employees table
+-- --------------------------------------------------
+insert into abrahan.employees values (1, 'Berrie', 'Manueau', 'bmanueau0@dion.ne.jp', '2006-04-20', 'Sports', 'F', 154864, 4);
+insert into abrahan.employees values (2, 'Aeriell', 'McNee', 'amcnee1@google.es', '2009-01-26', 'Tools', 'F', 56752, 3);
+insert into abrahan.employees values (3, 'Sydney', 'Symonds', 'ssymonds2@hhs.gov', '2010-05-17', 'Clothing', 'F', 95313, 4);
+insert into abrahan.employees values (4, 'Avrom', 'Rowantree', null, '2014-08-02', 'Phones & Tablets', 'M', 119674, 7);
+insert into abrahan.employees values (5, 'Feliks', 'Morffew', 'fmorffew4@a8.net', '2003-01-14', 'Computers', 'M', 55307, 5);
+insert into abrahan.employees values (6, 'Bethena', 'Trow', 'btrow5@technorati.com', '2003-06-08', 'Sports', 'F', 134501, 3);
+insert into abrahan.employees values (7, 'Ardeen', 'Curwood', 'acurwood6@1und1.de', '2006-02-19', 'Clothing', 'F', 28995, 7);
+insert into abrahan.employees values (8, 'Seline', 'Dubber', 'sdubber7@t-online.de', '2012-05-28', 'Phones & Tablets', 'F', 101066, 3);
+insert into abrahan.employees values (9, 'Dayle', 'Trail', 'dtrail8@tamu.edu', '2003-03-01', 'First Aid', 'F', 82753, 1);
+insert into abrahan.employees values (10, 'Redford', 'Roberti', null, '2008-07-21', 'Clothing', 'M', 72225, 7);
+insert into abrahan.employees values (11, 'Nickey', 'Pointon', 'npointona@vistaprint.com', '2006-12-30', 'Jewelry', 'M', 126333, 7);
+insert into abrahan.employees values (12, 'Leonora', 'Casaroli', 'lcasarolib@plala.or.jp', '2013-07-22', 'Beauty', 'F', 99504, 3);
+insert into abrahan.employees values (13, 'Anetta', 'Arnao', null, '2009-05-23', 'Games', 'F', 38162, 1);
+insert into abrahan.employees values (14, 'Jodi', 'Hook', 'jhookd@booking.com', '2003-10-16', 'Tools', 'F', 126588, 2);
+insert into abrahan.employees values (15, 'Alyson', 'Franzonello', null, '2004-01-01', 'Furniture', 'F', 61256, 6);
+insert into abrahan.employees values (16, 'Merell', 'Yakovliv', 'myakovlivf@ucsd.edu', '2008-08-16', 'Movies', 'M', 78141, 7);
+insert into abrahan.employees values (17, 'Annora', 'Bendelow', 'abendelowg@google.com.hk', '2009-06-12', 'Toys', 'F', 75283, 5);
+insert into abrahan.employees values (18, 'Ronica', 'Armfield', null, '2012-09-14', 'Toys', 'F', 114983, 5);
+insert into abrahan.employees values (19, 'Bernardine', 'Hendricks', 'bhendricksi@privacy.gov.au', '2006-02-04', 'Device Repair', 'F', 158268, 3);
+insert into abrahan.employees values (20, 'Jessey', 'Colum', 'jcolumj@pen.io', '2013-07-08', 'Computers', 'M', 77173, 7);
+insert into abrahan.employees values (21, 'Bernardo', 'Davage', null, '2013-07-11', 'Clothing', 'M', 124949, 6);
+insert into abrahan.employees values (22, 'Cayla', 'Duffer', null, '2007-02-01', 'Grocery', 'F', 78589, 3);
+insert into abrahan.employees values (23, 'Vanda', 'Marwick', 'vmarwickm@upenn.edu', '2014-02-06', 'Automotive', 'F', 103570, 4);
+insert into abrahan.employees values (24, 'Corabel', 'Syversen', 'csyversenn@aboutads.info', '2016-03-03', 'Pharmacy', 'F', 91323, 3);
+insert into abrahan.employees values (25, 'Zane', 'Breem', 'zbreemo@abc.net.au', '2012-07-30', 'Cosmetics', 'M', 56688, 6);
+insert into abrahan.employees values (26, 'Frasquito', 'Cawson', null, '2006-06-24', 'Movies', 'M', 78881, 1);
+insert into abrahan.employees values (27, 'Niles', 'Chawkley', 'nchawkleyq@flavors.me', '2013-09-22', 'Movies', 'M', 156303, 3);
+insert into abrahan.employees values (28, 'Emanuele', 'Sandcraft', 'esandcraftr@toplist.cz', '2015-01-01', 'Garden', 'M', 70827, 2);
+insert into abrahan.employees values (29, 'Cortie', 'Ambrosini', 'cambrosinis@microsoft.com', '2014-05-03', 'Games', 'M', 133847, 4);
+insert into abrahan.employees values (30, 'Benn', 'Dudley', 'bdudleyt@dailymail.co.uk', '2012-08-24', 'Beauty', 'M', 67800, 7);
+insert into abrahan.employees values (31, 'Cleve', 'Conti', 'ccontiu@noaa.gov', '2008-02-27', 'Garden', 'M', 91608, 5);
+insert into abrahan.employees values (32, 'Aldon', 'Paddison', 'apaddisonv@lulu.com', '2013-06-30', 'First Aid', 'M', 109592, 5);
+insert into abrahan.employees values (33, 'Ernestine', 'Sloley', 'esloleyw@rakuten.co.jp', '2011-06-27', 'Device Repair', 'F', 146549, 5);
+insert into abrahan.employees values (34, 'Lucy', 'Olifaunt', 'lolifauntx@dailymotion.com', '2005-02-07', 'Sports', 'F', 165660, 7);
+insert into abrahan.employees values (35, 'Olenolin', 'Blundan', null, '2004-09-29', 'Beauty', 'M', 137691, 3);
+insert into abrahan.employees values (36, 'Anatollo', 'Doyle', null, '2016-04-08', 'Computers', 'M', 98207, 1);
+insert into abrahan.employees values (37, 'Wilfrid', 'Sainer', 'wsainer10@gizmodo.com', '2015-09-30', 'Movies', 'M', 147235, 3);
+insert into abrahan.employees values (38, 'Edna', 'Erwin', null, '2003-04-09', 'Children Clothing', 'F', 91397, 5);
+insert into abrahan.employees values (39, 'Mikael', 'Kybbye', 'mkybbye12@si.edu', '2010-12-07', 'Vitamins', 'M', 52642, 1);
+insert into abrahan.employees values (40, 'Modestine', 'Proctor', 'mproctor13@digg.com', '2015-05-04', 'Tools', 'F', 81889, 7);
+insert into abrahan.employees values (41, 'Jania', 'Gresly', 'jgresly14@ted.com', '2010-09-08', 'Furniture', 'F', 97812, 6);
+insert into abrahan.employees values (42, 'Penelopa', 'Danieli', 'pdanieli15@microsoft.com', '2014-06-23', 'Toys', 'F', 85218, 1);
+insert into abrahan.employees values (43, 'Maurise', 'Bengefield', 'mbengefield16@sohu.com', '2005-07-08', 'Pharmacy', 'F', 63332, 2);
+insert into abrahan.employees values (44, 'Moise', 'Turpey', 'mturpey17@mac.com', '2008-04-15', 'Device Repair', 'M', 33781, 1);
+insert into abrahan.employees values (45, 'Giacopo', 'Mariner', 'gmariner18@noaa.gov', '2005-01-31', 'Grocery', 'M', 130445, 7);
+insert into abrahan.employees values (46, 'Lanny', 'Comolli', 'lcomolli19@posterous.com', '2014-06-19', 'Phones & Tablets', 'F', 34250, 6);
+insert into abrahan.employees values (47, 'Byrann', 'Jaukovic', 'bjaukovic1a@networkadvertising.org', '2011-11-13', 'Books', 'M', 121054, 7);
+insert into abrahan.employees values (48, 'Birgitta', 'Stanbrooke', 'bstanbrooke1b@netvibes.com', '2016-09-12', 'Clothing', 'F', 77259, 1);
+insert into abrahan.employees values (49, 'Portie', 'Ilden', 'pilden1c@usa.gov', '2006-01-20', 'Device Repair', 'M', 69190, 7);
+insert into abrahan.employees values (50, 'Modesty', 'Blagburn', 'mblagburn1d@studiopress.com', '2008-03-16', 'Beauty', 'F', 130580, 1);
